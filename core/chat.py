0b134b0246a837982827e58474ac5b4207a5169c
mrLevin778 <vitaliy.respect@gmail.com>
2023-07-07 20:16:50 +0300

    v0.0.2: changed class to Messaging, added send_message method

--- core/chat.py
+++ core/chat.py
@@ -1,24 +1,30 @@
 from matrix_client.client import MatrixClient
 
 
-class Auth:
+class Messaging:
 
-    def __init__(self, username, password, homeserver):
-        self.username = username
-        self.password = password
+    def __init__(self, homeserver):
         self.homeserver = homeserver
         self.client = None
 
-    def authenticate(self):
+    def connect(self, username, password):
         client = MatrixClient(self.homeserver)  # connect to Matrix server
         try:
-            client.login(username=self.username, password=self.password)  # try auth
-            print(f'Connecting to server {self.homeserver} for user {self.username} is OK')
+            client.login(username=username, password=password)  # try auth
+            print(f'Connecting to server {self.homeserver} for user {username} is OK')
         except Exception as e:
             try:
                 # if auth is wrong, try register
-                client.register_with_password(username=self.username, password=self.password)
+                client.register_with_password(username=username, password=password)
                 print(f'Registration and authentication is success!')
             except Exception as e:
                 print(f'Error in registration/authentication!')
-        return client
+        return self.client
+
+    def send_message(self, room_id, message):
+        if self.client:
+            room = self.client.rooms[room_id]
+            room.send_text(message)
+            print(f'Message sended!')
+        else:
+            print(f'Message is not sended!')
